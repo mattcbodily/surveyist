f6bc189af1896f9174375dcae9f0fb0b01518778
mattcbodily <mattcbodily@gmail.com>
2020-09-08 15:43:56 -0600

    sessions and massive setup, db connected

--- server/index.js
+++ server/index.js
@@ -6,5 +6,19 @@ const express = require('express'),
       app = express();
 
 app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
+}));
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected');
+})
 
 app.listen(SERVER_PORT, console.log(`Surveying on ${SERVER_PORT}`));
