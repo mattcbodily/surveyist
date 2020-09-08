bb5be6da2843f6e8d3733871aea8169d0cfd424c
mattcbodily <mattcbodily@gmail.com>
2020-09-08 16:22:39 -0600

    auth functionality complete on client-side

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default props => {
     let [email, setEmail] = useState(''),
         [password, setPassword] = useState('');
+
+    const login = (e) => {
+        e.preventDefault();
+        axios.post('/api/login', {email, password})
+        .then(res => {
+            //use context api here
+        })
+        .catch(err => console.log(err));
+    }
 
     return (
         <section>
@@ -13,7 +23,7 @@ export default props => {
                 <input value={email} onChange={e => setEmail(e.target.value)}/>
                 <label>Password</label>
                 <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
-                <button>Log In</button>
+                <button onClick={e => login(e)}>Log In</button>
             </form>
             <p>Don't have an account? <Link to='/register'>Sign up here</Link></p>
         </section>
--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default props => {
     let [username, setUsername] = useState(''),
@@ -7,6 +8,18 @@ export default props => {
         [password, setPassword] = useState(''),
         [verPassword, setVerPassword] = useState('');
         
+    const register = (e) => {
+        e.preventDefault();
+        if(password && password === verPassword){
+            axios.post('/api/register', {username, email, password})
+            .then(res => {
+                //use context api here
+            })
+            .catch(err => console.log(err));
+        } else {
+            alert("Passwords don't match")
+        }
+    }
 
     return (
         <section>
@@ -20,7 +33,7 @@ export default props => {
                 <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
                 <label>Verify Password</label>
                 <input type='password' value={verPassword} onChange={e => setVerPassword(e.target.value)}/>
-                <button>Register</button>
+                <button onClick={e => register(e)}>Register</button>
             </form>
             <p>Have an account? <Link to='/login'>Sign in here</Link></p>
         </section>
