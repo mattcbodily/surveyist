66867083250bc95e2afe0510c1bfebde98f9d080
mattcbodily <mattcbodily@gmail.com>
2020-09-09 16:24:57 -0600

    added basic styling to the SurveyDash

--- src/Components/SurveyDash/SurveyDash.js
+++ src/Components/SurveyDash/SurveyDash.js
@@ -1,14 +1,13 @@
 import React, {useContext} from 'react';
 import {UserContext} from '../../App';
+import './SurveyDash.scss';
 
 export default props => {
     const {user} = useContext(UserContext);
 
-    console.log(user)
-
     return (
-        <section>
-            Survey Dash
+        <section className='survey-dash'>
+            <h3>Your Surveys</h3>
         </section>
     )
 }
